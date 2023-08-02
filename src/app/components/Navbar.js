ac9f177f82f98a7a1123c5fbc73afb5c346d949f
SENTHIL M K <MSL12@dsone.3ds.com>
2023-08-02 13:15:20 +0530

    added routes and navbar updated

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,7 +1,28 @@
-export default function Navbar(){
+import Link from 'next/link'
+
+export default function Navbar() {
     return (
-        <div className=" bg-red-500">
-            <h1 >NavBar</h1>
+        <div className=" flex w-full bg-gray-400">
+            <div className="ml-2 self-center">
+                <h1 className="text-4xl text-center">Leslie</h1>
+            </div>
+            <ul className="flex w-full justify-end self-center mr-2">
+                <li className="mr-4">
+                    <Link href={"/"}>
+                        Home
+                    </Link>
+                </li>
+                <li className="mr-4">
+                    <Link href={"/about"}>
+                        About
+                    </Link>
+                </li>
+                <li className="mr-4">
+                    <Link href={"/contact"}>
+                        Contact
+                    </Link>
+                </li>
+            </ul>
         </div>
     )
 }

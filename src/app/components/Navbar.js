d691972d09f04d0c1db9f072fdc578e5889a6c94
SENTHIL M K <MSL12@dsone.3ds.com>
2023-08-02 15:25:45 +0530

    added selected button style

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,24 +1,32 @@
+"use client"
 import Link from 'next/link'
 
+import {useState} from 'react'
+
 export default function Navbar() {
+    const [activeButton, setActiveButton] = useState("home")
+
+
     return (
-        <div className=" flex w-full bg-gray-400">
-            <div className="ml-2 self-center">
-                <h1 className="text-4xl text-center">Leslie</h1>
+        <div className=" flex  w-full bg-gray-600 h-14 text-white">
+            <div className="ml-4 self-center ">
+                <Link href={"/"}>
+                    <h1 className="text-4xl text-center bg-black p-2 " onClick={()=>setActiveButton("home")} >LESLIE</h1>
+                </Link>
             </div>
-            <ul className="flex w-full justify-end self-center mr-2">
-                <li className="mr-4">
-                    <Link href={"/"}>
+            <ul className="flex w-full h-full justify-end items-center  text-xl ">
+                <li className={`flex mr-4 h-full px-2 ${activeButton=== "home" ? "bg-slate-900":"" }` } onClick={()=> setActiveButton("home")} >
+                    <Link href={"/"} className='self-center'>
                         Home
                     </Link>
                 </li>
-                <li className="mr-4">
-                    <Link href={"/about"}>
+                <li  className={` flex mr-4 h-full  px-2 ${activeButton=== "about" ? "bg-slate-900":"" }` } onClick={()=> setActiveButton("about")}>
+                    <Link href={"/about"} className='self-center'>
                         About
                     </Link>
                 </li>
-                <li className="mr-4">
-                    <Link href={"/contact"}>
+                <li  className={`flex  h-full  px-2 mr-2 ${activeButton=== "contact" ? "bg-slate-900":"" }` } onClick={()=> setActiveButton("contact")}>
+                    <Link href={"/contact"} className='self-center'>
                         Contact
                     </Link>
                 </li>
--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body >
+      <body className='w-screen' >
         <Navbar />
         {children}
       </body>
